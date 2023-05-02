32d9d11b340db4fc9685edfc4a0666fa2bcebd26
pyrosec <inquiries@pyrosec.gg>
2023-05-02 17:01:43 -0400

    update

--- src.ts/truepeoplesearch.ts
+++ src.ts/truepeoplesearch.ts
@@ -327,7 +327,7 @@ export class TruePuppeteer extends BasePuppeteer {
     return await this._resultWorkflow();
   }
   async _resultWorkflow() {
-    await this.waitForSelector({ selector: '#personDetails' });
+    await this.waitForSelector({ selector: 'div' });
     const result = await this.extractData();
     if (result && !result.person) return null;
     return result;
